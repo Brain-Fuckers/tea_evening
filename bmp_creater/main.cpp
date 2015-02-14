86d2dfd14347043ccc4b25e2429a298a6e85e199
OFedorov <offedoroff@yandex.ru>
2015-02-14 23:12:09 +0300

    More true code!

--- bmp_creater/main.cpp
+++ bmp_creater/main.cpp
@@ -14,12 +14,11 @@ typedef int LONG;
 typedef struct tagBITMAPFILEHEADER
  {
    WORD    bfType;
-   //char    T1,T2;
    DWORD   bfSize;
    DWORD    bfReserved1;
    //WORD    bfReserved2;
-
    DWORD   bfOffBits;
+
    //ushort   bfType;
    //uint     bfSize;
    //uint     bfReserved;
@@ -29,7 +28,6 @@ typedef struct tagBITMAPFILEHEADER
 
   typedef struct tagBITMAPINFOHEADER
  {
-     /*
    DWORD  biSize;
    LONG   biWidth;
    LONG   biHeight;
@@ -41,7 +39,7 @@ typedef struct tagBITMAPFILEHEADER
    LONG   biYPelsPerMeter;
    DWORD  biClrUsed;
    DWORD  biClrImportant;
-   */
+     /*
     uint  biSize;
     int   biWidth;
     int   biHeight;
@@ -53,107 +51,105 @@ typedef struct tagBITMAPFILEHEADER
     int   biYPelsPerMeter;
     uint  biClrUsed;
     uint  biClrImportant;
+   */
  } BITMAPINFOHEADER, *PBITMAPINFOHEADER;
 
+int createBmpFile(const char *path, int px, int py, unsigned short BitPerPixel, unsigned char *buf)
+{
+    BITMAPFILEHEADER bfh;
+    BITMAPINFOHEADER bih;
 
+    memset(&bfh, 0, sizeof(bfh));
+    memset(&bih, 0, sizeof(bih));
 
-     typedef struct tagBITMAPV5HEADER{
-      DWORD         bV5Size;
-      LONG          bV5Width;
-      LONG          bV5Height;
-      WORD          bV5Planes;
-      WORD          bV5BitCount;
-      DWORD         bV5Compression;
-      DWORD         bV5SizeImage;
-      LONG          bV5XPelsPerMeter;
-      LONG          bV5YPelsPerMeter;
-      DWORD         bV5ClrUsed;
-      DWORD         bV5ClrImportant;
+    int AddByte;
+    int BytePerPixel = BitPerPixel/8;
+    if      (BitPerPixel == 8 ) {AddByte = (3 * px) % 4;}
+    else if (BitPerPixel == 16) {AddByte = (2 * px) % 4;}
+    else if (BitPerPixel == 24) {AddByte =      px  % 4;}
+    else return -1;
 
-      DWORD         bV5RedMask;
-      DWORD         bV5GreenMask;
-      DWORD         bV5BlueMask;
-      DWORD         bV5AlphaMask;
-      DWORD         bV5CSType;
-//      CIEXYZTRIPLE  bV5Endpoints;
-      DWORD         bV5GammaRed;
-      DWORD         bV5GammaGreen;
-      DWORD         bV5GammaBlue;
-      DWORD         bV5Intent;
-      DWORD         bV5ProfileData;
-      DWORD         bV5ProfileSize;
-      DWORD         bV5Reserved;
-    } BITMAPV5HEADER, *PBITMAPV5HEADER;
+    bfh.bfType    = 0x4D42;
+    bfh.bfOffBits = sizeof(bfh) + sizeof(bih);
+    bfh.bfSize    = bfh.bfOffBits + //heder
+                    (BytePerPixel * px * py) + //data
+                    (AddByte * py);   // leveling bttes
 
+    bih.biSize        = sizeof(bih);
+    bih.biWidth       = px;
+    bih.biHeight      = py;
+    bih.biPlanes      = 1;
+    bih.biBitCount    = BitPerPixel;
+    bih.biCompression = 0; //BI_RGB
 
+    FILE *f = fopen( path, "wb" );
+	if( !f ) return -1;
+	size_t res;
+
+    res = fwrite(&bfh, sizeof(bfh), 1, f);
+    if (res != 1) return -1;
+    res = fwrite(&bih, sizeof(bih), 1, f);
+    if (res != 1) return -1;
+
+    for (int i = 0; i < py; i++)
+    {
+        fwrite(buf, px * BytePerPixel, 1, f);
+        buf+=px * BytePerPixel;
+        for (int j; j < AddByte; j)
+            fputc(0,f);
+    }
+    fclose(f);
+
+    return bfh.bfSize;
+}
+
+unsigned char *getRundBufRGB(int arrSize)
+{
+    unsigned char *buf = new unsigned char[arrSize];
+    for (int i = 0; i < arrSize; i++)
+    {
+        buf[i] = (unsigned char)(rand()*(200./RAND_MAX));
+    }
+    return buf;
+}
+
+unsigned char *getRundBufBW24(int arrSize)
+{
+    unsigned char *buf = new unsigned char[arrSize];
+    for (int i = 0; i < arrSize;)
+    {
+        unsigned char _cllor = (unsigned char)(rand()*(200./RAND_MAX));
+        buf[i++] = _cllor;
+        buf[i++] = _cllor;
+        buf[i++] = _cllor;
+    }
+    return buf;
+}
+
+unsigned char *getMandelbrotBuf(int x, int y, int BytePerPixel)
+{
+    return 0;
+}
 
 int main()
 {
 
-    cout << "sizeof WORD " << sizeof(WORD) << endl;
-    cout << "sizeof DWORD " << sizeof(DWORD) << endl;
-    cout << "sizeof LONG " << sizeof(LONG) << endl;
-
-    BITMAPFILEHEADER bfh;
-    BITMAPINFOHEADER bih;
-    cout << "sizeof bfh " << sizeof(bfh) << endl;
-    cout << "sizeof bih " << sizeof(bih) << endl;
-    char Palette[1024];
-
-    int px = 36;
-    int py = 35;
-
-    memset (Palette, 0, 1024);
-    memset (&bfh, 0, sizeof(bfh));
-    memset (&bih, 0, sizeof(bih));
-
-    bfh.bfType = 0x4D42;
-    //bfh.T1 = 'B';
-    //bfh.T2 = 'M';
-    bfh.bfOffBits = 14 + sizeof(bih);// + 1024;
-    bfh.bfSize = bfh.bfOffBits + 3 * px * py;
-        cout << "file size " << bfh.bfSize << endl;
-
-    bih.biSize = sizeof(bih);
-    bih.biWidth = px;
-    bih.biHeight = py;
-    bih.biPlanes = 1;
-    bih.biBitCount = 24;
-    bih.biCompression = 0;
-    bih.biSizeImage =  14+40+(3*36*35);
-    bih.biXPelsPerMeter = 0;
-    bih.biYPelsPerMeter = 0;
-    bih.biClrUsed = 0;
-    bih.biClrImportant = 0;
-
-
-
-    FILE *f = fopen( "test.bmp", "wb" );
-	if( !f ) return -1;
-	size_t res;
-
-    res = fwrite(&bfh, 14, 1, f);
-    cout << "res 1 " << res << endl;
-    res = fwrite(&bih, sizeof(bih), 1, f);
-    cout << "res 2 " << res << endl;
-
-    //char *h = (char) bfh;
-    //printf(h);
-    unsigned char buf[px*3][py];
-
-
-    for (int i = 0; i < py; i++)
+    unsigned char *buf1;
+    buf1 = new unsigned char[3*100 * 100];
+    int iii = 0;
+    for (int i = 0; i < 100*100*3; i++)
     {
-        for (int j = 0; j < px*3; j++)
-        {
-            buf[j][i] = 100;
-        }
+        buf1[iii] = (unsigned char)(rand()*(200./RAND_MAX));
+        iii++;
     }
-    cout << "size of buf " << sizeof(buf) << " ,and must be 3780" << endl;
-    res = fwrite(buf,sizeof(buf),1,f);
 
-    fclose(f);
 
+    unsigned char *buf = getRundBufBW24(30000);
+
+    cout << createBmpFile("fun1.bmp", 100, 100, 24, buf) << endl;
+
+    delete []buf1;
+    delete []buf;
     cout << "Hello world!" << endl;
     return 0;
 }
