3a757999d8070390ee31856b554c081caeb3e995
OFedorov <offedoroff@yandex.ru>
2015-02-17 22:50:32 +0300

    It will be programme for reading and writeing jpeg.

---
+++ jpegWR/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//http://habrahabr.ru/post/102521/
+//https://ru.wikipedia.org/wiki/JPEG
+//http://www.kolpinkurs.ru/stati/jpeg.htm
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
