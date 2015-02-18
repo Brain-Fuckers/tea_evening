0f7197286348c7df83e69ccdf022f0da4f5c32af
OFedorov <offedoroff@yandex.ru>
2015-02-18 11:46:20 +0300

    Add one link on en.wiki.

--- jpegWR/main.cpp
+++ jpegWR/main.cpp
@@ -2,6 +2,7 @@
 //http://habrahabr.ru/post/102521/
 //https://ru.wikipedia.org/wiki/JPEG
 //http://www.kolpinkurs.ru/stati/jpeg.htmcd
+//https://en.wikipedia.org/wiki/JPEG
 using namespace std;
 
 int main()
